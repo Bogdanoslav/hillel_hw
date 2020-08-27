5b4131d660e4525f88abe11fdcee47ba1d3243e0
Bogdanoslav <bvasenshev@gmail.com>
2020-08-27 13:07:18 +0300

    hw_15_fixed_tests

--- src/test/java/hw_9_Double_Linked_List/MyDoubleLinkedListTest.java
+++ src/test/java/hw_9_Double_Linked_List/MyDoubleLinkedListTest.java
@@ -2,6 +2,8 @@ package hw_9_Double_Linked_List;
 
 import junit.framework.TestCase;
 import org.junit.Before;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import static org.junit.Assert.*;
@@ -20,20 +22,23 @@ public class MyDoubleLinkedListTest extends TestCase {
         l_empty = new MyDoubleLinkedList();
         l_filled.addAll(arr);
     }
+
+    @Test
     public void testIsEmpty() {
         assertTrue(l_empty.isEmpty());
         assertFalse( l_filled.isEmpty());
     }
-
+    @Test
     public void testAdd() {
         assertTrue(l_empty.add("A"));
         assertTrue(l_filled.add("E"));
     }
-
+    @Test
     public void testAddAllArr() {
         assertTrue(l_filled.addAll(arr));
         assertTrue(l_empty.addAll(arr));
     }
+    @Test
     public void testAddAllCol() {
         col1 = new ArrayList<>();
         col1.add("D");
@@ -41,45 +46,49 @@ public class MyDoubleLinkedListTest extends TestCase {
         assertTrue(l_filled.addAll(col1));
         assertTrue(l_empty.addAll(col1));
     }
-
-
+    @Test
     public void testDeleteByIndex() {
         assertFalse(l_filled.delete(22));
         assertTrue(l_filled.delete(2));
     }
-
+    @Test
     public void testTestDelete() {
         assertFalse(l_filled.delete("Z"));
         assertTrue(l_filled.delete("A"));
     }
-
+    @Test
     public void testGet() {
         assertThat(l_filled.get(0), is("A"));
     }
 
+    @Test
     public void testContains() {
         assertTrue(l_filled.contains("B"));
         assertFalse(l_filled.contains("F"));
     }
 
+    @Test
     public void testClear() {
         assertTrue(l_empty.clear());
         assertTrue(l_filled.clear());
-        //Не понимаю какой вариант правильнее для тестов верхний или нижний ¯\_(ツ)_/¯
-        assertThat(l_empty.size(), is(0));
-        assertThat(l_filled.size(), is(0));
+        assertTrue(l_empty.isEmpty());
+        assertTrue(l_filled.isEmpty());
     }
 
+    @Test
     public void testSize() {
         assertThat(l_empty.size(), is(0));
         assertThat(l_filled.size(), is(3));
     }
+
+    @Test
     public void testTrim() {
         l_filled.add(null);
         l_filled.addAll(arr);
         assertTrue(l_filled.trim());
     }
 
+    @Test
     public void testCompare() {
         col1 = new ArrayList<>();
         MyDoubleLinkedList col2 = new MyDoubleLinkedList();
